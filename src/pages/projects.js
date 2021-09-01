3b5a322141fedeb1b35abb1b04f8e28d3597d1ba
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-09-01 19:59:47 +0530

    algolia search frontend setup

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,6 +13,7 @@ const ProjectsPage = ({ data }) => {
     <Wrapper>
       <Layout>
         <Projects title="our projects" projects={projects} page />
+        <Algolia />
       </Layout>
     </Wrapper>
   )
